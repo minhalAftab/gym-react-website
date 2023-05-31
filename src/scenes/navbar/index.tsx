9dfaee6c499f72ce63fe5feb4ffff6a28e528846
Minhal Aftab <minhalaftabrana@gmail.com>
2023-05-31 15:55:50 +0500

    working on navbar page links

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
-type Props = {};
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-function Navbar(props: Props) {
+function Navbar({ selectedPage, setSelectedPage }: Props) {
   const flexBetween = "flex items-center justify-between";
   return (
     <nav>
@@ -16,11 +20,31 @@ function Navbar(props: Props) {
             {/* RIGHT SIDE  */}
             <div className={`${flexBetween} w-full `}>
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>About</p>
-                <p>Benefits</p>
-                <p>Our Classes</p>
-                <p>Contact us</p>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="About"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign In</p>
